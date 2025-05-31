c1600d76ba91e0c9bedfdeb0564cfe835a3e42f0
Edki08 <droxdream@gmail.com>
2025-05-31 14:57:58 -0400

    Update logout.js

--- logout.js
+++ logout.js
@@ -1,19 +1,18 @@
 // logout.js
-import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { firebaseConfig } from "./firebase-config.js";
 
-// Initialize Firebase if not already initialized
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-// Call this function on logout button click
-export function logoutUser() {
+window.logoutUser = function () {
   signOut(auth)
     .then(() => {
+      alert("Logged out.");
       window.location.href = "login.html";
     })
     .catch((error) => {
       console.error("Logout error:", error);
     });
-}
+};

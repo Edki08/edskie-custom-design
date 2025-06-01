c822059478b27d019174db887e616dda1f911914
Edki08 <droxdream@gmail.com>
2025-06-01 11:48:17 -0400

    Add files via upload

--- background.js
+++ background.js
@@ -1,1 +1,7 @@
-// background.js content goes here
+const API_KEY = "eV273GPF1rVF3NNrcidPzIu2qN1XIkW21O8OouuZFEw";
+const query = "tshirts,graphic,urban";
+fetch(`https://api.unsplash.com/photos/random?query=${query}&client_id=${API_KEY}`)
+  .then(res => res.json())
+  .then(data => {
+    document.body.style.backgroundImage = `url(${data.urls.full})`;
+  });
